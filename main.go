e6dd664341f7fc8e6de89ca1c21e911a7fd7b2a8
hacel <s@sammar.sa>
2025-08-02 15:05:51 +0300

    feat: add version and help flags

--- main.go
+++ main.go
@@ -14,7 +14,22 @@ const (
 
 func main() {
 	cfgPath := pflag.StringP("config", "c", "", "override path to configuration file")
+	printVersion := pflag.BoolP("version", "v", false, "show version")
+	help := pflag.BoolP("help", "h", false, "show help")
 	pflag.Parse()
+
+	if *help {
+		println("Usage:  jfsh [OPTIONS]")
+		println()
+		println("Options:")
+		pflag.PrintDefaults()
+		return
+	}
+
+	if *printVersion {
+		println(clientVersion)
+		return
+	}
 
 	// first off, run a side bubbletea model that takes care of configuration and initializing the api client
 	client := config.Run(clientName, clientVersion, *cfgPath)
